1c9e6d788f8d36d2bfb9c02ceb69aaf22303651e
d0riven <wega315@gmail.com>
2017-05-07 22:12:04 +0900

    Change scratch.php to class base code, but doesn't use class field.

--- lib/InsertDataValidator/Column.php
+++ lib/InsertDataValidator/Column.php
@@ -3,6 +3,9 @@
 namespace InsertDataValidator;
 
 
+use InsertDataValidator\Exception\UnsupportedTypeException;
+use UnexpectedValueException;
+
 class Column
 {
     const TYPE_INTEGER   = 0;
@@ -81,4 +84,232 @@ class Column
         self::DATA_TYPE_TIME      => '-838:59:59',
         self::DATA_TYPE_TIMESTAMP => '1970-01-01 00:00:01',
     ];
+
+    public function extractDataType($schemaTypeOf)
+    {
+        if (preg_match('/^(.*?)(\(.+\))?$/', $schemaTypeOf, $matches)) {
+            return strtolower($matches[1]);
+        }
+        assert(false, 'Unexpected schemaType format. schemaType = ' . $schemaTypeOf);
+    }
+
+    public function extractMaxLength($schemaType)
+    {
+        if (preg_match('/^.*\((\d+)\).*$/', $schemaType, $matches)) {
+            return (int)$matches[1];
+        }
+        assert(false, sprintf('Logic error. type = %s', $schemaType));
+    }
+
+    public function isUnsigned($schemaTypeOf)
+    {
+        return stripos('unsigned', $schemaTypeOf) !== false;
+    }
+
+    function getRoughlyType($dataType)
+    {
+        if ($this->isTypeOfTinyInt($dataType) ||
+            $this->isTypeOfSmallInt($dataType) ||
+            $this->isTypeOfMediumInt($dataType) ||
+            $this->isTypeOfInt($dataType) ||
+            $this->isTypeOfBigInt($dataType)
+        ) {
+            return Column::TYPE_INTEGER;
+        }
+
+        if ($this->isTypeOfFloat($dataType) ||
+            $this->isTypeOfDouble($dataType) ||
+            $this->isTypeOfDecimal($dataType)
+        ) {
+            return Column::TYPE_DECIMAL;
+        }
+
+        if ($this->isTypeOfChar($dataType) ||
+            $this->isTypeOfVarChar($dataType)
+        ) {
+            return Column::TYPE_STRING;
+        }
+        if ($this->isTypeOfDate($dataType) ||
+            $this->isTypeOfDateTime($dataType) ||
+            $this->isTypeOfTime($dataType) ||
+            $this->isTypeOfTimeStamp($dataType) ||
+            $this->isTypeOfYear($dataType)
+        ) {
+            return Column::TYPE_DATE;
+        }
+        if (
+            $this->isTypeOfBit($dataType) ||
+            $this->isTypeOfBinary($dataType) ||
+            $this->isTypeOfVarBinary($dataType) ||
+            $this->isTypeOfTinyText($dataType) ||
+            $this->isTypeOfText($dataType) ||
+            $this->isTypeOfMediumText($dataType) ||
+            $this->isTypeOfLongText($dataType) ||
+            $this->isTypeOfTinyBinary($dataType) ||
+            $this->isTypeOfBlob($dataType) ||
+            $this->isTypeOfMediumBlob($dataType) ||
+            $this->isTypeOfLongBlob($dataType) ||
+            $this->isTypeOfEnum($dataType) ||
+            $this->isTypeOfSet($dataType)
+        ) {
+            // TODO Implements Unsupported Type Exception
+            throw new UnsupportedTypeException('Unsupported type. type = ' . $dataType);
+        }
+
+        throw new UnexpectedValueException('Unexpected value. type = ' . $dataType);
+    }
+
+    public function isTypeOfTinyInt($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_TINYINT;
+    }
+
+    public function isTypeOfSmallInt($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_SMALLINT;
+    }
+
+    public function isTypeOfMediumInt($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_MEDIUMINT;
+    }
+
+    public function isTypeOfInt($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_INT;
+    }
+
+    public function isTypeOfBigInt($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_BIGINT;
+    }
+
+    public function isTypeOfFloat($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_FLOAT;
+    }
+
+    public function isTypeOfDouble($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_DOUBLE;
+    }
+
+    public function isTypeOfDecimal($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_DECIMAL;
+    }
+
+    public function isTypeOfChar($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_CHAR;
+    }
+
+    public function isTypeOfVarChar($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_VARCHAR;
+    }
+
+    public function isTypeOfDate($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_DATE;
+    }
+
+    public function isTypeOfDateTime($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_DATETIME;
+    }
+
+    public function isTypeOfTime($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_TIME;
+    }
+
+    public function isTypeOfTimeStamp($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_TIMESTAMP;
+    }
+
+    public function isTypeOfYear($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_YEAR;
+    }
+
+    public function isTypeOfBit($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_BIT;
+    }
+
+    public function isTypeOfBinary($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_BINARY;
+    }
+
+    public function isTypeOfVarBinary($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_VARBINARY;
+    }
+
+    public function isTypeOfTinyText($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_TINYTEXT;
+    }
+
+    public function isTypeOfText($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_TEXT;
+    }
+
+    public function isTypeOfMediumText($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_MEDIUMTEXT;
+    }
+
+    public function isTypeOfLongText($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_LONGTEXT;
+    }
+
+    public function isTypeOfTinyBinary($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_TINYBLOB;
+    }
+
+    public function isTypeOfBlob($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_BLOB;
+    }
+
+    public function isTypeOfMediumBlob($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_MEDIUMBLOB;
+    }
+
+    public function isTypeOfLongBlob($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_LONGBLOB;
+    }
+
+    public function isTypeOfEnum($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_ENUM;
+    }
+
+    public function isTypeOfSet($dataType)
+    {
+        return $dataType === Column::DATA_TYPE_SET;
+    }
+
+    public function getMaxValue($dataType, $isUnsigned)
+    {
+        $maxValue = Column::MAX_VALUE[$dataType];
+        return $isUnsigned ? $maxValue * 2 + 1 : $maxValue;
+    }
+
+    public function getMinValue($dataType, $isUnsigned)
+    {
+        if ($isUnsigned) {
+            return 0;
+        }
+        return Column::MIN_VALUE[$dataType];
+    }
 }
--- lib/InsertDataValidator/Validator.php
+++ lib/InsertDataValidator/Validator.php
@@ -2,21 +2,111 @@
 
 namespace InsertDataValidator;
 
+use Exception;
+use InsertDataValidator\Exception\UnsupportedTypeException;
 use PDO;
+use Respect\Validation\Exceptions\ValidationException;
+use Respect\Validation\Validator as RespectValidation;
 
 class Validator
 {
-    public function validate(PDO $pdo, $tableName)
+    public function validate(PDO $pdo, $tableName, array $insertData, array $ignoreValidationColumns = [])
     {
-        $this->loadTableSchema($pdo, $tableName);
+        $errors = [];
+        $schema = $this->fetchTableSchema($pdo, $tableName);
+        // Format validation
+        foreach ($schema as $c) {
+            $columnName = $c['field'];
+            if (array_key_exists($columnName, $insertData) === false ||
+                in_array($columnName, $ignoreValidationColumns, true)
+            ) {
+                continue;
+            }
+            try {
+                $this->validateByColumnMetaData($c, $insertData[$columnName], $columnName);
+            } catch (ValidationException $e) {
+                $errors[] = sprintf('`%s` is not valid. reason = [%s]', $columnName, $e->getMainMessage());
+            }
+        }
+        // ghost column validation
+        $tableColumns = array_map(function ($c) {
+            return strtolower($c);
+        }, array_column($schema, 'field'));
+        $ghostColumns = array_diff(array_keys($insertData), $tableColumns);
+        if (count($ghostColumns) > 0) {
+            $errors[] = sprintf('Insert data has ghost column. ghosts = [`%s`]', implode('`,`', $ghostColumns));
+        }
+
+        if ($errors !== []) {
+            // TODO implements original Exception
+            throw new Exception(
+                sprintf('Exists violation of table schema. table_name = %s, errors = %s',
+                    $tableName, print_r($errors, true))
+            );
+        }
     }
 
-    public function loadTableSchema(PDO $pdo, $tableName)
+    public function fetchTableSchema(PDO $pdo, $tableName)
     {
-        $stmt = $pdo->query("show columns from $tableName");
-        $schema = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        foreach($schema as $columnMetaData) {
-            new Column($columnMetaData['field'], $columnMetaData['type'], $columnMetaData['null'], $columnMetaData['default']);
+        $stmt = $pdo->query("SHOW COLUMNS FROM $tableName");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function validateByColumnMetaData($metaData, $insertValue, $columnName)
+    {
+        //$c['field'], $c['type'], $c['null'], $c['default'];
+        /** @var RespectValidation $v */
+        $v = null;
+        // null許容のルール
+        if ($metaData['null'] === 'YES' && $insertValue === null) {
+            // データがnullならvalidationを掛けると逆に怒られるので早期リターン
+            return;
         }
+        if ($metaData['null'] === 'NO' && $insertValue === null) {
+            throw new ValidationException(sprintf('`%s` column must be not null', $columnName));
+        }
+        // ここから下はnullじゃないデータがはいってくるはず
+
+        // 型のルール
+        $schemaType = $metaData['type'];
+        $column = new Column();
+        $dataType = $column->extractDataType($schemaType);
+        try {
+            $type = $column->getRoughlyType($dataType);
+        } catch (UnsupportedTypeException $e) {
+            // memo この例外の処理は利用者に任せるべきか？
+            return;
+        }
+        $isUnsigned = $column->isUnsigned($schemaType);
+        switch ($type) {
+            case Column::TYPE_INTEGER:
+                $v = $column->extractMaxLength($schemaType) === 1 ? RespectValidation::boolVal() : RespectValidation::intVal();
+                break;
+            case Column::TYPE_DECIMAL:
+                $v = RespectValidation::floatVal();
+                break;
+            case Column::TYPE_STRING:
+                // 文字列の場合は文字列長のルールも規定に入れる
+                $v = RespectValidation::stringType()->length(null, $column->extractMaxLength($schemaType), true);
+                break;
+            case Column::TYPE_DATE:
+                $v = RespectValidation::date('Y-m-d');
+                break;
+            case Column::TYPE_DATETIME:
+            case Column::DATA_TYPE_TIMESTAMP:
+                $v = RespectValidation::date('Y-m-d H:i:s');
+                break;
+            case Column::TYPE_TIME:
+                $v = RespectValidation::date('H:i:s');
+                break;
+            case Column::TYPE_YEAR:
+                $v = $column->extractMaxLength($schemaType) === 4 ? RespectValidation::date('Y') : RespectValidation::date('y');
+                break;
+        }
+        assert($dataType !== null, 'Unexpected type. type = ' . $dataType);
+        // 値域のルール
+        $v->between($column->getMinValue($dataType, $isUnsigned), $column->getMaxValue($dataType, $isUnsigned), true);
+
+        $v->check($insertValue);
     }
 }
