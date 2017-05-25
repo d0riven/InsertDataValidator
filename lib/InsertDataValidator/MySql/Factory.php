43d11c2395a92621b6bdec04c6f56a3ca063e5e1
d0riven <wega315@gmail.com>
2017-05-26 08:11:25 +0900

    Fix bug : cannot get column meta data by character case.

--- lib/InsertDataValidator/MySql/Factory.php
+++ lib/InsertDataValidator/MySql/Factory.php
@@ -7,12 +7,13 @@ use InsertDataValidator\FactoryInterface;
 
 class Factory implements FactoryInterface
 {
-    const FIELD_COLUMN_NAME = 'Field';
-    const TYPE_COLUMN_NAME = 'Type';
-    const NULL_ALLOWABLE_COLUMN_NAME = 'Null';
+    const FIELD_COLUMN_NAME = 'field';
+    const TYPE_COLUMN_NAME = 'type';
+    const NULL_ALLOWABLE_COLUMN_NAME = 'null';
 
     public function createColumnMetaData(array $metaData)
     {
+        $metaData = array_change_key_case($metaData, CASE_LOWER);
         return new ColumnMetaData(
             $metaData[self::FIELD_COLUMN_NAME],
             $metaData[self::TYPE_COLUMN_NAME],
